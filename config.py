cb31451e8c8d69718e1377a55c8aed116e5c3387
ZahraMozafari23 <95679906+ZahraMozafari23@users.noreply.github.com>
2026-07-16 14:10:41 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+CHAT_ID2 = os.getenv("CHAT_ID2")
+
+API_URL = "https://www.ourbit.com/api/platform/spot/market/v2/tickers"
+
+DROP_PERCENT = -50
