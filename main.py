da790b3dd8f45c6cfb6acf4dceb491c91303bbde
ZahraMozafari23 <95679906+ZahraMozafari23@users.noreply.github.com>
2026-07-16 14:13:55 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,38 @@
-from confing import*
-print("bot start")
+from config import *
+import requests
+
+
+def get_market():
+
+    try:
+        response = requests.get(API_URL, timeout=20)
+
+        print("API Status:", response.status_code)
+
+        if response.status_code != 200:
+            print(response.text)
+            return None
+
+        return response.json()
+
+    except Exception as e:
+        print("API Error:", e)
+        return None
+
+
+print("Bot Started")
+
+data = get_market()
+
+if data:
+    coins = data["data"]
+
+    print("Coins:", len(coins))
+
+    for coin in coins[:5]:
+        print(
+            coin["sb"],
+            coin["c"]
+        )
+
+print("Finished")
