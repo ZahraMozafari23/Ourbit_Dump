7b3136efc0833e17c71424e1c238abe8328bb043
ZahraMozafari23 <95679906+ZahraMozafari23@users.noreply.github.com>
2026-07-21 12:17:04 +0330

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 CHAT_ID_2 = os.getenv("CHAT_ID_2")
+CHAT_ID_3 = os.getenv("CHAT_ID_3")
 
 API_URL = "https://www.ourbit.com/api/platform/spot/market/v2/tickers"
 
